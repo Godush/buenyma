db4929d85b0c0cccbad8efa923b8567f3e685461
Marius Treu <marius.treu@gmail.com>
2012-10-01 21:21:46 +0200

    - dropdown beim Menü (game.php, menu.php, menu.js)

--- script/js/menuSlider.js
+++ script/js/menuSlider.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$("div#kontrolle").mouseover(function () {
 		$(this).find(".menuPoint").stop(true,true).slideDown();
 	});
@@ -20,5 +20,16 @@ $(document).ready(function() {
 	});
 	$("div#diplomatie").mouseleave(function () {
 		$(this).find(".menuPoint").stop(true,true).fadeOut();
+	});
+});*/
+$(document).ready(function() {
+	$maxHeight=$("div.menuPointCon").height();
+	$("div.menuPointCon").css("height","0");
+	$("div.menuPointCon").css("opacity","0");
+	$("div.menuCon").mouseover(function () {
+		$(this).children("div.menuPointCon").stop().animate({"opacity":"1","height":""+$maxHeight+"px"},{duration:"medium"});
+	})
+	$("div.menuCon").mouseleave(function () {
+		$(this).children("div.menuPointCon").stop().animate({"opacity":"0","height":"0px"},{duration:"slow"});
 	});
 });
